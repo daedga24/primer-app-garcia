71681ace710a7ec341ffdaebf3e2c232c5d20e2a
Julian Garcia <daedga38@gmail.com>
2020-06-23 15:08:28 -0300

    modificaciones para Heroku

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(3000);
+app.listen(port);
